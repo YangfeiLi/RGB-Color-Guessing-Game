05cca3de73613706621056bce1724aa735363d01
Yangfei Li <lyfelmer@outlook.com>
2017-11-26 12:24:18 -0500

    random color function

--- RGBCGG.js
+++ RGBCGG.js
@@ -1,15 +1,9 @@
-var colors = [
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-var messageDisplay = document.querySelector("#message")
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +18,7 @@ for (var i = 0; i < squares.length; i++) {
 		if(clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.background = clickedColor;
 		} else {
 			this.style.background = "#232323";
 			messageDisplay.textContent = "Try Again"
@@ -36,4 +31,34 @@ function changeColors(color) {
 	for (var i = 0; i < squares.length; i ++) {
 		squares[i].style.background = color;
 	}
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	var arr = [];
+	// repeat num times
+	for(var i = 0; i < num; i ++) {
+		arr.push(randomColor())
+	}
+
+	return arr;
+}
+
+function randomColor() {
+	// pick a "R" from  0 - 255
+	var r =  Math.floor(Math.random() * 256);
+
+	// pick a "G" from  0 - 255
+	var g =  Math.floor(Math.random() * 256);
+
+	// pick a "B" from  0 - 255
+	var b =  Math.floor(Math.random() * 256);
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+
+
 }
